c27ebbb423a3844a2b8008143c43306efdf5491c
keroti <peterkeroti55@gmail.com>
2023-05-25 19:23:59 +0300

    Regexing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,5 +14,4 @@ def filter_datum(
     use a regex to replace occurrences of certain field values
     and returns the log message obfuscated.
     """
-    data = r'({}=)[^{}]+'.format('|'.join(fields), separator)
-    return re.sub(data, r'\1' + redaction, message)
+    return re.sub(r'({})=[^{}]+'.format('|'.join(fields), separator), r'\1' + redaction, message)
