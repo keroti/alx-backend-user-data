a64538dc8ac85c80a7014186ff2dc3b8be0d4c1f
keroti <peterkeroti55@gmail.com>
2023-05-25 19:19:56 +0300

    Regexing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,10 @@ Module for filtering log data
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """
     use a regex to replace occurrences of certain field values
     and returns the log message obfuscated.
