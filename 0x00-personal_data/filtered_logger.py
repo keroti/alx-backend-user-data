4a6bac6e3bfc0c08383941d0ebecd101a5642581
keroti <peterkeroti55@gmail.com>
2023-05-25 19:27:27 +0300

    Regexing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,12 +7,10 @@ import re
 from typing import List
 
 
-def filter_datum(
-        fields: List[str], redaction: str, message: str, separator: str
-) -> str:
+def filter_datum(fields, redaction, message, separator):
     """
     use a regex to replace occurrences of certain field values
     and returns the log message obfuscated.
     """
-    pattern = r'({}=)[^{}]+'.format('|'.join(fields), separator)
-    return re.sub(pattern, r'\1' + redaction, message)
+    pattern = r'({0}=).*?({1})'.format('|'.join(fields), separator)
+    return re.sub(pattern, r'\1{0}'.format(redaction), message)
