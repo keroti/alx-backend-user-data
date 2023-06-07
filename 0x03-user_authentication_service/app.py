1560241f2999ba9d98fc40ae8d09a2a6c50d694a
keroti <peterkeroti55@gmail.com>
2023-06-07 12:31:02 +0300

    Register User

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -17,19 +17,19 @@ def welcome():
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route("/users", methods=["POST"])
-def register_user():
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
     """
-    Implement endpoint to register user
+    Implement endpoint to register new user
     """
     email = request.form.get("email")
     password = request.form.get("password")
-
     try:
         user = AUTH.register_user(email, password)
-        return jsonify({"email": user.email, "message": "user created"})
-    except ValueError as err:
-        return jsonify({"message": str(err)}), 400
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": f"{email}", "message": "user created"})
 
 
 if __name__ == "__main__":
