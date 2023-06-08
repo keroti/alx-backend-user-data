c963f9ec2b9c04b5eedaa0e0b70a6b6194bda56f
keroti <peterkeroti55@gmail.com>
2023-06-08 18:46:55 +0300

    Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Flask app module
 """
-from flask import Flask, jsonify, request, make_response, redirect
+from flask import Flask, jsonify, request, make_response, redirect, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -44,7 +44,7 @@ def login():
         return response
     else:
         return make_response("Unauthorized", 401)
-    
+
 
 @app.route('/sessions', methods=['DELETE'])
 def logout():
@@ -56,7 +56,6 @@ def logout():
         if user:
             AUTH.destroy_session(user.id)
             return redirect('/')
-    
     return jsonify(message='Forbidden'), 403
 
 
@@ -71,6 +70,44 @@ def profile():
             return jsonify(email=user.email), 200
 
     return jsonify(message='Forbidden'), 403
+
+
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """POST /reset_password
+    Return:
+        - The user's password reset payload.
+    """
+    email = request.form.get("email")
+    reset_token = None
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        reset_token = None
+    if reset_token is None:
+        abort(403)
+    return jsonify({"email": email, "reset_token": reset_token})
+
+
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> str:
+    """PUT /reset_password
+
+    Return:
+        - The user's password updated payload.
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    is_password_changed = False
+    try:
+        AUTH.update_password(reset_token, new_password)
+        is_password_changed = True
+    except ValueError:
+        is_password_changed = False
+    if not is_password_changed:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"})
 
 
 if __name__ == "__main__":
